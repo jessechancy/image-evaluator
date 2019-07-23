b86fab7898d6700800ab56e7e557ad620bbed550
jzchan34 <jessec1@andrew.cmu.edu>
2019-07-23 11:59:20 +0800

    Reorganized Files / Added CNN Code

---
+++ networks/image-preprocessing.py
@@ -0,0 +1,10 @@
+import cv2
+import os
+
+for user in directory:
+    user = []
+    for img in user:
+        cv2.read(img)
+        img_name = os.img_name
+        name, likes, comments, date, time = img_name[], img_name[], img_name[]
+        
